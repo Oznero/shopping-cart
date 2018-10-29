cdb685897188556544fede46442e3e910e13e0f3
oznero <kcroom@gmail.com>
2018-10-29 11:48:48 -0400

    updated files

---
+++ assets/javascript/buttonList.js
@@ -0,0 +1,51 @@
+var buttonList = [
+    {
+        name: 'Apple Laptop',
+        category: 'Technology'
+    },
+    {
+        name: 'Apple iPhone',
+        category: 'Technology'
+    },
+    {
+        name: 'Apple Watch',
+        category: 'Technology'
+    },
+    {
+        name: 'Windows Laptop',
+        category: 'Technology'
+    },
+    {
+        name: 'Bananas',
+        category: 'Grocery'
+    },
+    {
+        name: 'Apples',
+        category: 'Grocery'
+    },
+    {
+        name: 'Juice',
+        category: 'Grocery'
+    },
+    {
+        name: 'Eggs',
+        category: 'Grocery'
+    },
+    {
+        name: 'Jackets',
+        category: 'Clothing'
+    },
+    {
+        name: 'Seaters',
+        category: 'Clothing'
+    },
+    {
+        name: 'T-Shirts',
+        category: 'Clothing'
+    },
+    {
+        name: 'Tennis Shoes',
+        category: 'Clothing'
+    },
+
+];
